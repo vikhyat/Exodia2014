3de27bfdd74949476c8477d0bb177dc6ae4e0284
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-03 09:50:31 +0530

    initial commit.

---
+++ source/javascripts/application.js
@@ -0,0 +1,15 @@
+//= require vendor/jquery
+//= require vendor/jquery.cookie
+//= require vendor/modernizr
+//= require vendor/fastclick
+//
+//= require foundation/foundation
+//= require foundation/foundation.offcanvas
+//
+// Development only -- remove later!
+//
+//= require vendor/placeholder
+
+$(function() {
+  $(document).foundation();
+});
