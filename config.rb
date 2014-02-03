3d00d0e9df7b5296a13bc205a478aca4c87765c5
Vikhyat Korrapati <vikhyatk@gmail.com>
2014-02-03 10:57:35 +0530

    ...

--- config.rb
+++ config.rb
@@ -28,6 +28,10 @@
 # proxy "/this-page-has-no-template.html", "/template-file.html", :locals => {
 #  :which_fake_page => "Rendering a fake page with a local variable" }
 
+data.events.map {|x| x[1] }.flatten.each do |event|
+  proxy "/about/events/#{event.id}/index.html", "/events/template.html"
+end
+
 ###
 # Helpers
 ###
