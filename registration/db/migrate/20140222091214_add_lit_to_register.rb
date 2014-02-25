ed29b753c5caa6ab0f7ceb88b8e6bfc32b98d778
Devang Bacharwar <devanggtgr8@gmail.com>
2014-02-25 15:47:20 +0530

    Accomodation added

--- registration/db/migrate/20140222091214_add_lit_to_register.rb
+++
@@ -1,8 +0,0 @@
-class AddLitToRegister < ActiveRecord::Migration
-  def change
-    add_column :registers, :liar, :boolean
-    add_column :registers, :devil, :boolean
-    add_column :registers, :gow, :boolean
-    add_column :registers, :survivor, :boolean
-  end
-end
--- registration/db/migrate/20140222091432_add_gaming_to_register.rb
+++
@@ -1,7 +0,0 @@
-class AddGamingToRegister < ActiveRecord::Migration
-  def change
-    add_column :registers, :cs, :boolean
-    add_column :registers, :fifa, :boolean
-    add_column :registers, :nfs, :boolean
-  end
-end
